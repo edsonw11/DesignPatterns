37ff4b21d0dcbcd9700bfecc639b5cc08f426a3d
Edson Batista da silva <edsonw11@gmail.com>
2012-12-01 19:12:50 -0200

    DP -Strategy

---
+++ src/br/com/dp/strategy/DPStrategy.java
@@ -0,0 +1,10 @@
+package br.com.dp.strategy;
+
+public class DPStrategy {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Design Patterns para bons programadores - Strategy");
+	}
+
+}
