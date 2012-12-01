c007080dac2e5946dc6c9b5d6af3b18eb90d8fd6
Edson <Edson@Edson-PC>
2012-12-01 19:22:14 -0200

    criando DP-ChanOfResponsibility via windows

---
+++ src/br/com/dp/chanofresponsibility/ChanOfResponsibility.java
@@ -0,0 +1,12 @@
+package br.com.dp.chanofresponsibility;
+
+public class ChanOfResponsibility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Design Patterns para bons programadores - ChanOfResponsibility");
+	}
+
+}
